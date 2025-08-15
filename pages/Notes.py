9f882cb1e55611c5aa188e9753cc6ce060494318
nav343 <snmlogic111@gmail.com>
2025-08-15 10:33:21 +0530

    Fixed a minor bug

--- pages/Notes.py
+++ pages/Notes.py
@@ -1,6 +1,5 @@
 import os
 import pickle
-from re import search
 import time
 from utils.colors import COLORS
 from utils.window import Window
@@ -25,9 +24,9 @@ def readNotes(window: Window) -> int:
                 counter += 1
             window.print()
         else:
-            window.print(
-                "You don't have any Notes.\nCreate one now?", color=COLORS.LIGHT_BLUE
-            )
+            window.print("You don't have any Notes", color=COLORS.LIGHT_BLUE)
+            window.print("Redirecting...", color=COLORS.LIGHT_RED)
+            time.sleep(2)
 
     except Exception as E:
         window.print("An unexpected error occurred. Please try again")
@@ -72,37 +71,43 @@ def Notes(window: Window) -> None:
                 window.rerender()
                 numberOfNotes = readNotes(window)
                 # window.print(f"There are {numberOfNotes} notes")
-                choice = ""
-                while True:
-                    try:
-                        choice = int(
-                            window.input(
-                                "Which Note do you want to open?", color=COLORS.GREEN
+                if numberOfNotes != 0:
+                    choice = ""
+                    while True:
+                        try:
+                            choice = int(
+                                window.input(
+                                    "Which Note do you want to open?",
+                                    color=COLORS.GREEN,
+                                )
                             )
-                        )
-                        if choice > numberOfNotes:
-                            window.print(
-                                f"Invalid index. You only have {numberOfNotes} notes.",
-                                color=COLORS.RED,
-                            )
-                        else:
-                            break
-                    except ValueError:
-                        window.print("Invalid. Try again", color=COLORS.RED)
+                            if choice > numberOfNotes:
+                                window.print(
+                                    f"Invalid index. You only have {numberOfNotes} notes.",
+                                    color=COLORS.RED,
+                                )
+                            else:
+                                break
+                        except ValueError:
+                            window.print("Invalid. Try again", color=COLORS.RED)
 
-                try:
-                    noteFile = open(f"tests/Note_{searchNotes()[choice - 1]}.dat", "rb")
-                    data = pickle.load(noteFile)["content"]
-                    window.rerender()
-                    window.print(searchNotes()[choice - 1], color=COLORS.LIGHT_BLUE)
-                    window.print("-" * (len(searchNotes()[choice - 1])))
-                    window.print()
-                    window.print(data)
-                    window.print()
-                    window.input("Press any key to go back.", color=COLORS.LIGHT_RED)
-                except Exception as E:
-                    window.print(str(E))
-                    time.sleep(2)
+                    try:
+                        noteFile = open(
+                            f"tests/Note_{searchNotes()[choice - 1]}.dat", "rb"
+                        )
+                        data = pickle.load(noteFile)["content"]
+                        window.rerender()
+                        window.print(searchNotes()[choice - 1], color=COLORS.LIGHT_BLUE)
+                        window.print("-" * (len(searchNotes()[choice - 1])))
+                        window.print()
+                        window.print(data)
+                        window.print()
+                        window.input(
+                            "Press any key to go back.", color=COLORS.LIGHT_RED
+                        )
+                    except Exception as E:
+                        window.print(str(E))
+                        time.sleep(2)
             case 2:
                 window.rerender()
                 createNotes(window)
